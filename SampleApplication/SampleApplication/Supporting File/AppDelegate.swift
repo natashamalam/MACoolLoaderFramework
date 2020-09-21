c7052ec54b6529bec268c5f54bd179fdadc22005
Mahjabin Alam <natasha.mahjabin@gmail.com>
2020-09-21 20:42:04 +0900

    Added a sample application placeholder

---
+++ SampleApplication/SampleApplication/Supporting File/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  SampleApplication
+//
+//  Created by Mahjabin Alam on 2020/09/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
